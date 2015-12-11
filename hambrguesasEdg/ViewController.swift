8946b4634c9010390666574eb2bc8b2a04f3d737
Alexbarron06 <alexandro.barron@iest.edu.mx>
2015-12-10 19:49:58 -0600

    Initial Commit

---
+++ hambrguesasEdg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hambrguesasEdg
+//
+//  Created by Alexandro Barron  on 10/12/15.
+//  Copyright © 2015 EdgarHT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
